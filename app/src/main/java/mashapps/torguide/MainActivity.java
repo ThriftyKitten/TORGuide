1749f3e8a60beea2795e99287244dc5cbe862726
Michael Scofield <michaelscofswork@gmail.com>
2019-02-22 20:21:34 -0800

    Adjusted Theme, Added World page. Constrained Buttons.

--- app/src/main/java/mashapps/torguide/MainActivity.java
+++ app/src/main/java/mashapps/torguide/MainActivity.java
@@ -1,7 +1,10 @@
 package mashapps.torguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void goWorld(View view) {
+        Intent intent = new Intent(this, WorldActivity.class);
+        startActivity(intent);
     }
 }
